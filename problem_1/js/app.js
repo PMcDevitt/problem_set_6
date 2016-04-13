53b90948861f8f25363214d1d0cdc17ead91a7c7
CompoZedANI01 <compozedani01@gmail.com>
2016-04-13 16:38:01 +0100

    provide solution for day8 problem 1 and 2

--- problem_1/js/app.js
+++ problem_1/js/app.js
@@ -1,3 +1,34 @@
 window.onload = function () {
-  // your JavaScript here
+  var button = document.getElementById('calculateBtn')
+  var textInput = document.getElementById('billAmount')
+  var selectInput = document.getElementById('tipOption')
+
+  button.addEventListener('click', updateDOM)
+  textInput.addEventListener('keyup', updateDOM)
+  selectInput.addEventListener('change', updateDOM)
 };
+
+// pure function
+function calculateTip(total, tipPercentage){
+  return total*tipPercentage
+}
+
+// pure function
+function getBillAmount(){
+  var totalAmount = document.getElementById('billAmount').value
+    if(totalAmount[0]==='$'){
+      return totalAmount.substring(1,totalAmount.length)
+    }
+    return totalAmount
+}
+
+// pure function
+function getTipValue(){
+  return document.getElementById('tipOption').options[tipOption.selectedIndex].value
+}
+
+// non pure function
+function updateDOM(){
+  var tip = calculateTip(getBillAmount(), getTipValue())
+  document.getElementById('tipAmount').innerHTML = 'You should tip $' + tip.toFixed(2)
+}
